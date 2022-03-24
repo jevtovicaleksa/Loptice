8ee7afb1b696dfceb2194e0f4bae2b20f128c01d
Jevtovic Aleksa <69771033+jevtovicaleksa@users.noreply.github.com>
2022-03-24 15:52:21 +0100

    Validacija forme

--- loptice.js
+++ loptice.js
@@ -1,6 +1,11 @@
 const brojLoptica = 24;
 const main = document.querySelector(".main");
+const forma = document.querySelector(".forma");
+const naslovBoje = document.getElementById("naslovBoje");
 const boje = ["#0984e3", "#d63031", "#ffeaa7", "#00b894", "#a29bfe"];
+const naizmenicneBoje = boje.sort((a, b) => 0.5 - Math.random());
+let oblik = "oblikPravougaonik";
+
 
 // Prikazujemo loptice dinamicki kroz for petlju
 for (let i = 0; i <= brojLoptica; i++) {
@@ -8,7 +13,7 @@ for (let i = 0; i <= brojLoptica; i++) {
   const randomBoja = boje[randomBroj];
   let lopta = document.createElement("div");
   lopta.classList.add("container");
-  lopta.innerHTML = `<div class="loptica" style="background-color: ${randomBoja}" data-color="${randomBoja}"></div>`;
+  lopta.innerHTML = `<div class="${oblik}" style="background-color: ${randomBoja}" data-color="${randomBoja}"></div>`;
   main.append(lopta);
 }
 
@@ -22,32 +27,17 @@ for (let i = 0; i < boje.length; i++) {
 }
 
 // Prolazimo kroz sve loptice. Za svaku lopticu proverimo da li se boje poklapaju. Ukoliko se poklapaju povecamo odredjenu boju za jedan
-const svaPolja = document.querySelectorAll(".loptica");
+const svaPolja = document.querySelectorAll(`.${oblik}`);
 svaPolja.forEach(function (jednoPolje) {
   for (let i = 0; i < boje.length; i++) {
     if (jednoPolje.dataset.color === boje[i]) {
       kolicinaBoja[i]++;
     }
   }
-
   // Prelaskom misa preko loptice pozivamo f-ju koja brise tu lopticu.
-  let pocetnoVreme = 5;
-  let a = 5;
   jednoPolje.addEventListener("mouseover", function () {
-    pomocnaSekunde++;
     obrisiLoptu(jednoPolje);
     prikazatiBoju();
-    if (pomocnaSekunde === 1) {
-      pocetnoVreme = new Date();
-      // console.log(pocetnoVreme);
-      a = 10;
-    } else if (pomocnaSekunde === svaPolja.length) {
-      console.log(pomocnaSekunde);
-      console.log(pocetnoVreme);
-      console.log();
-    }
-    // console.log(pocetnoVreme);
-    // console.log(a);
   });
 });
 
@@ -85,3 +75,56 @@ const prikazatiBoju = function () {
 // Prikazuje mi prvu boju koju treba da uklonim
 const trenutnaBoja = document.querySelector(".trenutnaBoja");
 trenutnaBoja.style.backgroundColor = `${boje[pomocnaBoja]}`;
+
+
+// Prikupljanje podataka iz forme i smestanje u localStorage
+document.querySelector(".prijava").addEventListener("click", function () {
+  if(validacijaForme() === true){
+    forma.classList.add("hidden");
+    main.classList.remove("hidden");
+    naslovBoje.classList.remove("hidden");
+  
+    const ime = document.getElementById("ime").value;
+    const prezime = document.getElementById("prezime").value;
+    const oblikFigure = document.querySelector(
+      'input[name="figura"]:checked'
+    ).value;
+  
+    // Smestanje podataka u localStorage
+    let podaciLocalStorage = [];
+    let korisnik = {
+      "ime" : ime,
+      "prezime" : prezime,
+      "oblik" : oblikFigure
+    }
+  
+    podaciLocalStorage.push(korisnik);
+    // let korisnici = localStorage.setItem('korisnici', JSON.stringify(podaciLocalStorage));
+  }
+});
+
+// F-ja za proveru forme.
+const validacijaForme = function() {
+  // Prikupljamo iz html elemente(polja) kako bi proverili da li su popunjena
+  const ime = document.getElementById("ime").value;
+  const prezime = document.getElementById("prezime").value;
+  const figura = document.querySelector('input[name="figura"]:checked');
+// Ukoliko polja nisu popunjena prikazujemo preko labele poruku. Uklanjamo joj klasu hidden kako bi bila vidljiva.
+  if(ime.trim().length === 0){
+    document.querySelector('.imeValidacija').classList.remove("hidden");
+    console.log('1');
+  }else if(prezime.trim().length === 0){
+    document.querySelector(".prezimeValidacija").classList.remove("hidden");
+    console.log(2);
+  }else if(figura === null){
+    document.querySelector(".figuraValidacija").classList.remove("hidden");
+    console.log(3);
+  }
+
+// Ukoliko prodjemo formu (sva polja budu popunjena f-ja nam vraca true). 
+  if(ime.trim().length != 0 && prezime.trim().length != 0 && figura != null ){
+    return true;
+  }else{
+    return false;
+  }
+}
