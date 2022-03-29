ae18ac30afbc2b95f755e476ef03d76a6384153a
Jevtovic Aleksa <69771033+jevtovicaleksa@users.noreply.github.com>
2022-03-29 16:00:48 +0200

    Postavljanje odgovarajuce figure

--- forma.js
+++ forma.js
@@ -1,38 +1,39 @@
+
 // Prikupljanje podataka iz forme i smestanje u localStorage
 document.querySelector(".prijava").addEventListener("click", function () {
-  oblikFigure();
-    if(validacijaForme() === true){
-      pocetnoVreme = new Date();
-      forma.classList.add("hidden");
-      main.classList.remove("hidden");
-      naslovBoje.classList.remove("hidden");
+      tipFigure();
+      if(validacijaForme() === true){
+        pocetnoVreme = new Date();
+        forma.classList.add("hidden");
+        main.classList.remove("hidden");
+        naslovBoje.classList.remove("hidden");
 
-      const ime = document.getElementById("ime").value;
-      const prezime = document.getElementById("prezime").value;
-      const oblikFigure = document.querySelector(
-        'input[name="figura"]:checked'
-      ).value;
-    
-      // Prikupljamo podatke iz LocalStorage-a i smestamo u promenljivu ls
-      let ls = localStorage.getItem('korisnici');
-      // Podatke iz forme smestamo u korisnika
-      let korisnik = {
-            "ime" : ime,
-            "prezime" : prezime,
-            "oblik" : oblikFigure
+        const ime = document.getElementById("ime").value;
+        const prezime = document.getElementById("prezime").value;
+        const oblikFigure = document.querySelector(
+          'input[name="figura"]:checked'
+        ).value;
+      
+        // Prikupljamo podatke iz LocalStorage-a i smestamo u promenljivu ls
+        let ls = localStorage.getItem('korisnici');
+        // Podatke iz forme smestamo u korisnika
+        let korisnik = {
+              "ime" : ime,
+              "prezime" : prezime,
+              "oblik" : oblikFigure
+          }
+
+          //ako posotji item u storage-u prikupljamo te podatke i na te podatke smestamo nove koji dolaze iz forme
+        if(ls != null) {
+          let korisnici = JSON.parse(ls);
+          korisnici.push(korisnik);
+          localStorage.setItem('korisnici', JSON.stringify(korisnici));
+        }else {
+          // prvi put koristimo formu(aplikaciju) na ovom kompu. Pravimo niz u koji smestamo podatke iz forme a onda iz niza stavljamo u storage
+          let korisnici = [];
+          korisnici.push(korisnik);
+          localStorage.setItem('korisnici', JSON.stringify(korisnici));
         }
-
-        //ako posotji item u storage-u prikupljamo te podatke i na te podatke smestamo nove koji dolaze iz forme
-      if(ls != null) {
-        let korisnici = JSON.parse(ls);
-        korisnici.push(korisnik);
-        localStorage.setItem('korisnici', JSON.stringify(korisnici));
-      }else {
-        // prvi put koristimo formu(aplikaciju) na ovom kompu. Pravimo niz u koji smestamo podatke iz forme a onda iz niza stavljamo u storage
-        let korisnici = [];
-        korisnici.push(korisnik);
-        localStorage.setItem('korisnici', JSON.stringify(korisnici));
-      }
     }
   });
   
@@ -42,6 +43,7 @@ document.querySelector(".prijava").addEventListener("click", function () {
     const ime = document.getElementById("ime").value;
     const prezime = document.getElementById("prezime").value;
     const figura = document.querySelector('input[name="figura"]:checked');
+    console.log(figura.value);
   // Ukoliko polja nisu popunjena prikazujemo preko labele poruku. Uklanjamo joj klasu hidden kako bi bila vidljiva.
     if(ime.trim().length === 0){
       document.querySelector('.imeValidacija').classList.remove("hidden");
@@ -60,9 +62,15 @@ document.querySelector(".prijava").addEventListener("click", function () {
   }
 
   // Oblik figure u zavisnosti od selektovanog polja
-
-  const oblikFigure = function(){
+  const tipFigure = function(){
     let figura = document.querySelector('input[name="figura"]:checked');
-    oblik = figura.value;
+    let novioblik = figura.value;
+    console.log(oblik);
+
+    document.querySelectorAll('.'+oblik).forEach(function(stariOblik){
+      stariOblik.classList.remove(oblik);
+      stariOblik.classList.add(novioblik);
+    })
   }
-  console.log(oblik);
+
+  
--- loptice.js
+++ loptice.js
@@ -5,106 +5,107 @@ const naslovBoje = document.getElementById("naslovBoje");
 // const boje = ["#0984e3", "#d63031", "#ffeaa7", "#00b894", "#a29bfe"];
 const boje = ["#0984e3", "#d63031"];
 const naizmenicneBoje = boje.sort((a, b) => 0.5 - Math.random());
-let oblik = "oblikPravougaonik";
 
-// Setujemo promenljive koje koristimo za izracunavanje vremena. Prosledjujemo ih funkciji
-let pocetnoVreme = 0;
-let krajnjeVreme = 0;
-// let ukupnoVreme = 0;
+  let oblik = 'oblikPravougaonik';
 
-// Setujemo promenljivu koju koristimo za promenu boje
-let pomocnaBoja = 0;
-
-// Prikazujemo loptice dinamicki kroz for petlju
-for (let i = 0; i <= brojLoptica; i++) {
-  const randomBroj = Math.floor(Math.random() * boje.length);
-  const randomBoja = boje[randomBroj];
-  let lopta = document.createElement("div");
-  lopta.classList.add("container");
-  lopta.innerHTML = `<div class="${oblik}" style="background-color: ${randomBoja}" data-color="${randomBoja}"></div>`;
-  main.append(lopta);
-}
-
-// Kreiramo prazan niz u koji smestamo onoliko elemenata koliko ima boja
-let kolicinaBoja = [];
-for (let i = 0; i < boje.length; i++) {
-  kolicinaBoja.push(0);
-}
-
-// Prolazimo kroz sve loptice. Za svaku lopticu proverimo da li se boje poklapaju. Ukoliko se poklapaju povecamo odredjenu boju za jedan
-const svaPolja = document.querySelectorAll(`.${oblik}`);
-svaPolja.forEach(function (jednoPolje) {
-  for (let i = 0; i < boje.length; i++) {
-    if (jednoPolje.dataset.color === boje[i]) {
-      kolicinaBoja[i]++;
-    }
+  // Setujemo promenljive koje koristimo za izracunavanje vremena. Prosledjujemo ih funkciji
+  let pocetnoVreme = 0;
+  let krajnjeVreme = 0;
+  // let ukupnoVreme = 0;
+  
+  // Setujemo promenljivu koju koristimo za promenu boje
+  let pomocnaBoja = 0;
+  
+  // Prikazujemo loptice dinamicki kroz for petlju
+  for (let i = 0; i <= brojLoptica; i++) {
+    const randomBroj = Math.floor(Math.random() * boje.length);
+    const randomBoja = boje[randomBroj];
+    let lopta = document.createElement("div");
+    lopta.classList.add("container");
+    lopta.innerHTML = `<div class="${oblik}" style="background-color: ${randomBoja}" data-color="${randomBoja}"></div>`;
+    main.append(lopta);
   }
-  // Prelaskom misa preko loptice pozivamo f-ju koja brise tu lopticu.
-  jednoPolje.addEventListener("mouseover", function () {
-    obrisiLoptu(jednoPolje, pocetnoVreme);
-    prikazatiBoju();
-  });
-});
-
-// Funkcija za brisanje loptice. Ukoliko se boja loptice poklapa sa redosledom boje iz niza obrisace se loptica.
-const obrisiLoptu = function (loptica, pocetak) {
-  if (loptica.dataset.color === boje[pomocnaBoja]) {
-    loptica.remove();
-    kolicinaBoja[pomocnaBoja]--;
-    if (kolicinaBoja[pomocnaBoja] === 0) {
-      pomocnaBoja++;
-      if (pomocnaBoja >= boje.length) {
-        krajnjeVreme = new Date();
-        let ukupnoVreme = Math.abs((krajnjeVreme - pocetak) / 1000 );
-
-        // Dopunjavanje u localstorage vreme za svakog korisnika.
-        
-        // Prikuoljamo podatke koji se vec nalaze u ls, prilikom popunjavanja forme
-         let lStorage = JSON.parse(localStorage.getItem('korisnici'));
-
-        // Poslednjem korisniku dodajemo vreme za koje je preso igricu
-         lStorage[lStorage.length -1]['vreme'] = ukupnoVreme;
-         localStorage.setItem('korisnici',JSON.stringify(lStorage));
-         alert(`Vase vreme je ${ukupnoVreme} sekundi`)
-         window.location.reload();
+  
+  // Kreiramo prazan niz u koji smestamo onoliko elemenata koliko ima boja
+  let kolicinaBoja = [];
+  for (let i = 0; i < boje.length; i++) {
+    kolicinaBoja.push(0);
+  }
+  
+  // Prolazimo kroz sve loptice. Za svaku lopticu proverimo da li se boje poklapaju. Ukoliko se poklapaju povecamo odredjenu boju za jedan
+  const svaPolja = document.querySelectorAll(`.${oblik}`);
+  svaPolja.forEach(function (jednoPolje) {
+    for (let i = 0; i < boje.length; i++) {
+      if (jednoPolje.dataset.color === boje[i]) {
+        kolicinaBoja[i]++;
       }
     }
-  } else {
-    alert("Boja nije odgovarajuca");
-    window.location.reload(); // restartuje mi stranicu
-  }
-};
-
-// Trenutna boja. Funkcija koja se poziva prilikom prelaska misa preko nekog elementa
-const prikazatiBoju = function () {
-  const trenutnaBoja = document.querySelector(".trenutnaBoja");
-  let trBoja = document.createElement("div");
-  trBoja.classList.add("trBoja");
-  trBoja.style.backgroundColor = `${boje[pomocnaBoja]}`;
-  //ukoliko postoje child elementi u divu pod klasom trenutna boja ukloni mi prvi element
-  if (trenutnaBoja.hasChildNodes()) {
-    trenutnaBoja.removeChild(trenutnaBoja.children[0]);
-  }
-  trenutnaBoja.append(trBoja);
-};
-
-// Prikazuje mi prvu boju koju treba da uklonim
-const trenutnaBoja = document.querySelector(".trenutnaBoja");
-trenutnaBoja.style.backgroundColor = `${boje[pomocnaBoja]}`;
-
-
-//Dodavanje podataka iz localStorage u tabelu 
-let podaciLS = JSON.parse(localStorage.getItem('korisnici'));
-if(podaciLS != null){
-  console.log(podaciLS);
-  const teloTabele = document.getElementById('teloTabele');
-  podaciLS.forEach(podatakIgraca => {
-  console.log(podatakIgraca);
-  teloTabele.innerHTML += ` 
-                      <tr>
-                        <td>${podatakIgraca.ime}</td>
-                        <td>${podatakIgraca.prezime}</td>
-                        <td>${podatakIgraca.vreme}</td>
-                      </tr> `;
+    // Prelaskom misa preko loptice pozivamo f-ju koja brise tu lopticu.
+    jednoPolje.addEventListener("mouseover", function () {
+      obrisiLoptu(jednoPolje, pocetnoVreme);
+      prikazatiBoju();
+    });
   });
-}
+  
+  // Funkcija za brisanje loptice. Ukoliko se boja loptice poklapa sa redosledom boje iz niza obrisace se loptica.
+  const obrisiLoptu = function (loptica, pocetak) {
+    if (loptica.dataset.color === boje[pomocnaBoja]) {
+      loptica.remove();
+      kolicinaBoja[pomocnaBoja]--;
+      if (kolicinaBoja[pomocnaBoja] === 0) {
+        pomocnaBoja++;
+        if (pomocnaBoja >= boje.length) {
+          krajnjeVreme = new Date();
+          let ukupnoVreme = Math.abs((krajnjeVreme - pocetak) / 1000 );
+  
+          // Dopunjavanje u localstorage vreme za svakog korisnika.
+          
+          // Prikuoljamo podatke koji se vec nalaze u ls, prilikom popunjavanja forme
+           let lStorage = JSON.parse(localStorage.getItem('korisnici'));
+  
+          // Poslednjem korisniku dodajemo vreme za koje je preso igricu
+           lStorage[lStorage.length -1]['vreme'] = ukupnoVreme;
+           localStorage.setItem('korisnici',JSON.stringify(lStorage));
+           alert(`Vase vreme je ${ukupnoVreme} sekundi`)
+           window.location.reload();
+        }
+      }
+    } else {
+      alert("Boja nije odgovarajuca");
+      window.location.reload(); // restartuje mi stranicu
+    }
+  };
+  
+  // Trenutna boja. Funkcija koja se poziva prilikom prelaska misa preko nekog elementa
+  const prikazatiBoju = function () {
+    const trenutnaBoja = document.querySelector(".trenutnaBoja");
+    let trBoja = document.createElement("div");
+    trBoja.classList.add("trBoja");
+    trBoja.style.backgroundColor = `${boje[pomocnaBoja]}`;
+    //ukoliko postoje child elementi u divu pod klasom trenutna boja ukloni mi prvi element
+    if (trenutnaBoja.hasChildNodes()) {
+      trenutnaBoja.removeChild(trenutnaBoja.children[0]);
+    }
+    trenutnaBoja.append(trBoja);
+  };
+  
+  // Prikazuje mi prvu boju koju treba da uklonim
+  const trenutnaBoja = document.querySelector(".trenutnaBoja");
+  trenutnaBoja.style.backgroundColor = `${boje[pomocnaBoja]}`;
+  
+  //Dodavanje podataka iz localStorage u tabelu 
+  let podaciLS = JSON.parse(localStorage.getItem('korisnici'));
+  if(podaciLS != null){
+    console.log(podaciLS);
+    const teloTabele = document.getElementById('teloTabele');
+    podaciLS.forEach(podatakIgraca => {
+    console.log(podatakIgraca);
+    teloTabele.innerHTML += ` 
+                        <tr>
+                          <td>${podatakIgraca.ime}</td>
+                          <td>${podatakIgraca.prezime}</td>
+                          <td>${podatakIgraca.vreme}</td>
+                        </tr> `;
+    });
+  }
+
